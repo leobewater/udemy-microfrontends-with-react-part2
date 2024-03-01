e05711596e25e6f85232851b72f07f4496b39b51
leobewater <mmbee888@gmail.com>
2024-03-01 10:54:34 -0500

    add webpack.common to marketing

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 Video: https://www.udemy.com/course/microfrontend-course/learn/lecture/23206942
 
+This projects contains 4 modules
+- Container - React
+- Marketing - React
+- Auth - React
+- Dashboard - Vue
+
 # To Run
 ```bash
 
---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    rules: [
+      // process .mjs or .js by babel excluding node_modules folders
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
