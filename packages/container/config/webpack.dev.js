b51fc6a883c33a0059a623341006112e6b536f8d
leobewater <mmbee888@gmail.com>
2024-03-01 19:41:55 -0500

    use packagejson.dependencies to shared packages

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,6 +2,8 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+// use package.json dependencies to import packages from other modules' packages
+const packageJson = require('../package.json')
 
 const devConfig = {
   mode: "development",
@@ -17,7 +19,7 @@ const devConfig = {
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
-      shared: ["react", "react-dom"],
+      shared: packageJson.dependencies,
     }),
     // insert bundled js to public/index.html
     new HtmlWebpackPlugin({
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -2,6 +2,8 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+// use package.json dependencies to shared packages
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -18,7 +20,9 @@ const devConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
-      shared: ["react", "react-dom"],
+      // may not want to use packageJson if you want to have total control then use [] or {}
+      // see part1 code
+      shared: packageJson.dependencies,
     }),
     // insert bundled js to public/index.html
     new HtmlWebpackPlugin({
