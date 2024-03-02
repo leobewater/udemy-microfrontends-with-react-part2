0b3ac1e20501684ac3302c7923f80db753bffe39
leobewater <mmbee888@gmail.com>
2024-03-02 11:16:14 -0500

    add publicPath in marketing prod config

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,9 @@ const packageJson = require("../package.json");
 const prodConfig = {
   mode: "production",
   output: {
-    filename: '[name].[contenthash].js'
+    filename: "[name].[contenthash].js",
+    // match AWS S3/marketing/latest/ folder structure setting
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
