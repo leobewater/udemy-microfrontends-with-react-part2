fc9d24c3574626e046a8f17dc30e3e6381f822a3
leobewater <mmbee888@gmail.com>
2024-03-02 14:10:25 -0500

    copy and update auth webpack configs

---
+++ packages/auth/config/webpack.common.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      // process .mjs or .js by babel excluding node_modules folders
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    // insert bundled js to public/index.html
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
