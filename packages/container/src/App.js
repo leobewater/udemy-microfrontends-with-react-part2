ffa6479fe1647e444149b71490603c2a321ad94b
leobewater <mmbee888@gmail.com>
2024-03-02 15:29:14 -0500

    set up auth app in container and add initial path to avoid double click

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 import MarketingApp from "./components/MarketingApp";
+import AuthApp from "./components/AuthApp";
 import Header from "./components/Header";
 
 // add prefix for production css to avoid auto generated conflicts
@@ -18,7 +19,11 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            {/* path value matches the first part of the path */}
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
         </div>
       </StylesProvider>
     </BrowserRouter>
