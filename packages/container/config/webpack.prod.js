8028bb347536a30507e3df4d23826270fac30477
leobewater <mmbee888@gmail.com>
2024-03-02 08:49:40 -0500

    add container webpack production config

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,28 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+// use package.json dependencies to import packages from other modules' packages
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    // set up the output files with cache hash
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        // assume the remoteEntry is inside the "marketing" folder
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+// merge common and prod config (overriding common config if conflict)
+module.exports = merge(commonConfig, prodConfig);
