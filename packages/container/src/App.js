650767ade6ac0b75be5b78df24de5921cfe66b86
leobewater <mmbee888@gmail.com>
2024-03-02 22:03:45 -0500

    add onSignOut

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -24,7 +24,10 @@ export default () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header isSignedIn={isSignedIn} />
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
               {/* path value matches the first part of the path */}
